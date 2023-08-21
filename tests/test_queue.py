d405869dd5af9a37efc3ba07e5e2eec4d00a2d12
Johnny <evgkisevgkis@gmail.com>
2023-08-21 21:35:25 +0500

    Написание тестов к Queue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import *
+
+
+class TestStack(unittest.TestCase):
+    def test_queue(self):
+        test1 = Queue
+        self.assertTrue(test1)
+
+    def test_str(self):
+        test = Queue
+        self.assertTrue(str(test), "")
