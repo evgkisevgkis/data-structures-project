817efca04568f373243b31739ce3a4fb2c2f0707
Johnny <evgkisevgkis@gmail.com>
2023-09-19 22:10:22 +0500

    Добавлены тесты для класса Linked List

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_ll(self):
+        test = LinkedList()
+        test.insert_beginning(1)
+        self.assertEqual(test.head.data, 1)
+
+    def test_multiple(self):
+        test = LinkedList()
+        for n in range(5):
+            test.insert_beginning(n)
+        self.assertEqual(test.head.data, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
