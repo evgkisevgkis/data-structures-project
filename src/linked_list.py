13147a708bb370f9c614844a2aa4709314724bc5
Johnny <evgkisevgkis@gmail.com>
2023-09-19 21:51:06 +0500

    Написание класса Linked List

--- src/linked_list.py
+++ src/linked_list.py
@@ -7,14 +7,28 @@ class Node:
 
 class LinkedList:
     """Класс для односвязного списка"""
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data)
+        if self.head:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+        if self.tail:
+            self.tail.next_node = new_node
+            self.tail = new_node
+        else:
+            self.tail = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,10 +36,10 @@ class LinkedList:
         if node is None:
             return str(None)
 
-        ll_string = ''
+        ll_string = ""
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f"{str(node.data)} -> "
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += "None"
         return ll_string
